e6dff50a7efc05e88ddcbd327e248a0f4f39da58
Plat <github@p1at.dev>
2022-08-22 03:41:41 +0900

    feat: link card icon

--- utils/linkCard.ts
+++ utils/linkCard.ts
@@ -4,11 +4,33 @@ import { unfurl } from "unfurl.js"
 export const createLinkCard = async (url: string) => {
     const ogp = await unfurl(url)
 
-    console.log(ogp)
+    // console.log(ogp)
+
+    const twitterImage = (() => {
+        if (ogp.twitter_card !== undefined) {
+            if (ogp.twitter_card.images !== undefined) {
+                return ogp.twitter_card.images[0]
+            }
+        }
+
+        return undefined
+    })()
+    const openGraphImage = (() => {
+        if (ogp.open_graph !== undefined) {
+            if (ogp.open_graph.images !== undefined) {
+                return ogp.open_graph.images[0]
+            }
+        }
+
+        return undefined
+    })()
+
+    const image = twitterImage || openGraphImage || ogp.favicon || ""
+
+    console.log(image)
 
     const card = `
     <a href="${url}" targe="__blank" class="link-card">
-
         <div>
             <p class="title">${ogp.title ?? "No Title"}</p>
             <p class="url">${url}</p>
