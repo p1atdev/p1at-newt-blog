d1f2024bd73cc8e271708f560271945f94e2e654
Plat <github@p1at.dev>
2022-08-23 02:44:07 +0900

    chore: remove export option

--- components/sideNavBar/SideNavBar.tsx
+++ components/sideNavBar/SideNavBar.tsx
@@ -13,8 +13,7 @@ const ExternalLink = ({ href, icon, alt }: ExternalLinkProps) => {
     return (
         <div className="group relative flex w-6 flex-col items-center">
             <a href={href} target="__blank" className="w-6">
-                {/* <Image */}
-                <img
+                <Image
                     src={icon}
                     width={20}
                     height={20}
@@ -78,8 +77,7 @@ const SideNavBar = () => {
                     <Link href="/">
                         <a>
                             <div className="m-auto aspect-square rounded-full bg-blue-100 p-1.5 text-[0px] tracking-normal">
-                                {/* <Image */}
-                                <img
+                                <Image
                                     src={Static.ProfileURL}
                                     alt="icon"
                                     width={100}
