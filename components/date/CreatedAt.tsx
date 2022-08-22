b0f3a11ed16df40530577c5f083b630665ec06af
Plat <github@p1at.dev>
2022-08-23 04:31:11 +0900

    fix: hydration error with date formatting

--- components/date/CreatedAt.tsx
+++ components/date/CreatedAt.tsx
@@ -1,16 +1,23 @@
 import { format } from "date-fns"
 import { ja } from "date-fns/locale"
 import { Icon } from "@iconify/react"
+import { useEffect, useState } from "react"
 
 interface Props {
     date: string
 }
 
 const CreatedAt = ({ date }: Props) => {
+    const [formatedDate, setFormatedDate] = useState("")
+
+    useEffect(() => {
+        setFormatedDate(format(new Date(date), "yyyy/MM/dd", { locale: ja }))
+    }, [])
+
     return (
         <div className="flex items-center gap-x-1 text-gray-400">
             <Icon icon="akar-icons:newspaper" />
-            <p>{format(new Date(date), "yyyy/MM/dd", { locale: ja })}</p>
+            <p>{formatedDate}</p>
         </div>
     )
 }
